a854dc810bd50d1394da6a9ef15fa9c7933e46a8
B1on1cBen <bengg2015@gmail.com>
2018-02-15 15:09:01 -0500

    Added things

---
+++ SocketIOProject/SocketIOProject/Assets/Network.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using SocketIO;
+
+public class Network : MonoBehaviour {
+    static SocketIOComponent socket;
+    [SerializeField] private GameObject playerPrefab;
+
+	void Start () {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+        socket.On("spawn player", OnSpawn);
+	}
+
+    void OnConnected(SocketIOEvent e)
+    {
+        Debug.Log("Player connected!");
+        socket.Emit("playerhere");
+    }
+
+    void OnSpawn(SocketIOEvent e)
+    {
+        Debug.Log("Player spawned!");
+        Instantiate(playerPrefab);
+    }
+}
